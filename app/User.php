4ed5859c95447b6e2db8f2a15ea4b35ffe173fe0
Rauan Karibzhanov <irolik90@gmail.com>
2019-12-20 14:57:00 +0600

    update

--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ namespace App;
 
 use App\Notifications\MailResetPasswordNotification;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Support\Facades\DB;
 use Zizaco\Entrust\Traits\EntrustUserTrait;
