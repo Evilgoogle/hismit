35fc34cc2cda9ce900beecded418165ae9054c6f
Rauan Karibzhanov <irolik90@gmail.com>
2020-03-24 20:06:54 +0600

    mix fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/vendor.js', 'public/js')
-    .js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/vendor.scss', 'public/css')
-    .sass('resources/sass/app.scss', 'public/css')
+mix.js('resources/assets/js/vendor.js', 'public/js')
+    .js('resources/assets/js/app.js', 'public/js')
+    .sass('resources/assets/sass/vendor.scss', 'public/css')
+    .sass('resources/assets/sass/app.scss', 'public/css')
     .version();
