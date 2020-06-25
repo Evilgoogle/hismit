8f349ee2d75a145bc02b15ed1dd34273dc9eb245
Rauan Karibzhanov <irolik90@gmail.com>
2020-06-25 11:38:21 +0600

    upd

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,8 @@ const mix = require('laravel-mix');
 
 mix.js('resources/assets/js/vendor.js', 'public/js')
     .js('resources/assets/js/app.js', 'public/js')
+    .js('resources/assets/js/admin.js', 'public/js')
     .sass('resources/assets/sass/vendor.scss', 'public/css')
     .sass('resources/assets/sass/app.scss', 'public/css')
+    .sass('resources/assets/sass/admin.scss', 'public/css')
     .version();
