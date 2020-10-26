af7791057a5691a20b918d4ab0a2885e263e6c10
Rauan Karibzhanov <irolik90@gmail.com>
2020-10-26 16:20:00 +0600

    add slug function

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -165,3 +165,58 @@ if (!function_exists('getArrayFromJsonCollection')) {
         return $array;
     }
 }
+
+if (!function_exists('url_slug')) {
+    function url_slug($str, $options = array())
+    {
+        // Make sure string is in UTF-8 and strip invalid UTF-8 characters
+        $str = mb_convert_encoding((string)$str, 'UTF-8', mb_list_encodings());
+
+        $defaults = array(
+            'delimiter' => '-',
+            'limit' => null,
+            'lowercase' => true,
+            'replacements' => array(),
+            'transliterate' => false,
+        );
+
+        // Merge options
+        $options = array_merge($defaults, $options);
+
+        $char_map = array(
+            // Russian
+            'А' => 'A', 'Б' => 'B', 'В' => 'V', 'Г' => 'G', 'Д' => 'D', 'Е' => 'E', 'Ё' => 'Yo', 'Ж' => 'Zh',
+            'З' => 'Z', 'И' => 'I', 'Й' => 'J', 'К' => 'K', 'Л' => 'L', 'М' => 'M', 'Н' => 'N', 'О' => 'O',
+            'П' => 'P', 'Р' => 'R', 'С' => 'S', 'Т' => 'T', 'У' => 'U', 'Ф' => 'F', 'Х' => 'H', 'Ц' => 'C',
+            'Ч' => 'Ch', 'Ш' => 'Sh', 'Щ' => 'Sh', 'Ъ' => '', 'Ы' => 'Y', 'Ь' => '', 'Э' => 'E', 'Ю' => 'Yu',
+            'Я' => 'Ya',
+            'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'yo', 'ж' => 'zh',
+            'з' => 'z', 'и' => 'i', 'й' => 'j', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o',
+            'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c',
+            'ч' => 'ch', 'ш' => 'sh', 'щ' => 'sh', 'ъ' => '', 'ы' => 'y', 'ь' => '', 'э' => 'e', 'ю' => 'yu',
+            'я' => 'ya',
+            // Kazakh
+            'Ғ' => 'G', 'Ә' => 'A', 'Қ' => 'K', 'Ң' => 'N', 'Ө' => 'O', 'Ұ' => 'Y', 'Ү' => 'U', 'Һ' => 'H', 'І' => 'I',
+            'ғ' => 'g', 'ә' => 'a', 'қ' => 'k', 'ң' => 'n', 'ө' => 'o', 'ұ' => 'y', 'ү' => 'u', 'һ' => 'h', 'і' => 'i',
+        );
+
+        // Make custom replacements
+        $str = preg_replace(array_keys($options['replacements']), $options['replacements'], $str);
+        
+        $str = str_replace(array_keys($char_map), $char_map, $str);
+
+        // Replace non-alphanumeric characters with our delimiter
+        $str = preg_replace('/[^\p{L}\p{Nd}]+/u', $options['delimiter'], $str);
+
+        // Remove duplicate delimiters
+        $str = preg_replace('/(' . preg_quote($options['delimiter'], '/') . '){2,}/', '$1', $str);
+
+        // Truncate slug to max. characters
+        $str = mb_substr($str, 0, ($options['limit'] ? $options['limit'] : mb_strlen($str, 'UTF-8')), 'UTF-8');
+
+        // Remove delimiter from ends
+        $str = trim($str, $options['delimiter']);
+
+        return $options['lowercase'] ? mb_strtolower($str, 'UTF-8') : $str;
+    }
+}
